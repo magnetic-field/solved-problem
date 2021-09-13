de3a924f7d6bca6f6c8233eff46b418bf0029096
Sohan Reza <38208272+magnetic-field@users.noreply.github.com>
2021-09-13 18:42:02 +0600

    CSES Problem Set Permutation

--- main.cpp
+++ main.cpp
@@ -4,37 +4,34 @@ using namespace std;
 
 
 int main(void) {
-	
-	//
-	//freopen("./input", "r", stdin);
+
 
 	int n;
 	cin >> n;
 	
-	int arr[n];
+	if(n == 1) {cout << "1"; exit(0);}
 	
-	for(int i=0; i<n; i++) {
-		cin >> arr[i];
-	}
-	long long int counter = 0;
-	for(int i=0; i<n; i++) {
-		if(i != (n-1)) {
-				if(arr[i] > arr[i+1]) {
-						int def = (arr[i] - arr[i+1]);
-						arr[i+1] += def;
-						counter +=def;
-						//cout << def   <<endl;
-				}
-		}else {
-				//cout << "Last :" << i <<endl; 
-		}
+	if(n == 2 || n== 3) { cout << "NO SOLUTION" <<	endl; return 0;}
+	
+	if(n%2==0) {
+	
+	for(int i=2; i <=n; i+=2) {
+			cout << i <<" ";
 	}
 	
-	/*for(int i=0; i<n; i++) {
-			cout << arr[i] << " ";
-	}*/
-	cout << counter <<endl;
+	for(int i=1; i <=n; i+=2) {
+			cout << i <<" ";
+	}
+	}else {
+		for(int i=1; i <=n; i+=2) {
+			cout << i <<" ";
+	}
 	
+		for(int i=2; i <=n; i+=2) {
+				cout << i <<" ";
+		}	
+	}
+
 	return 0;
 	
 }
