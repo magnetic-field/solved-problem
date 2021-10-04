b60e65c27465ab677466755c9eb8724f42c22097
Sohan Reza <38208272+magnetic-field@users.noreply.github.com>
2021-10-04 10:53:11 +0600

    CSES Palindrome Reorder

--- main.cpp
+++ main.cpp
@@ -1,39 +1,67 @@
 #include <bits/stdc++.h>
-
+#include <array>
 using namespace std;
+
 
 
 int main(void) {
 	
-	//
-	//freopen("./input", "r", stdin);
-
-	int n;
-	cin >> n;
 	
-	int arr[n];
+	int list[26]={0};
+	string s;
+	cin >> s;
 	
-	for(int i=0; i<n; i++) {
-		cin >> arr[i];
+	for(unsigned int i=0; i<s.size(); i++){
+		list[(int)s[i] - 65] +=1;
 	}
-	long long int counter = 0;
-	for(int i=0; i<n; i++) {
-		if(i != (n-1)) {
-				if(arr[i] > arr[i+1]) {
-						int def = (arr[i] - arr[i+1]);
-						arr[i+1] += def;
-						counter +=def;
-						//cout << def   <<endl;
+	
+	int counter=0;
+	int k=0;
+	
+	int odd=0;
+	int car;
+	
+	
+	char arr[s.size()];
+	for(unsigned int i=0; i<s.size(); i++) {
+			arr[i] = 0;
+	}
+	
+	for(int i=0; i<26; i++) {
+			if(list[i] % 2 == 0) {
+					k++;
+			}else {
+					counter++;
+					k++;
+			}
+	}
+	
+	if(counter > 1) {cout << "NO SOLUTION" <<endl;return 0;}
+	int c=0;
+	for(int i=0; i<26; i++) {
+			if(list[i] > 0) {
+				if(list[i] %2 == 0) {
+						int b=list[i]/2;
+						for(int j=0; j<b; j++){arr[c++] = i+65;	cout << (char)(i+65);}
+				}else {
+						
+						car = i;
+						odd = list[i];
+						
 				}
-		}else {
-				//cout << "Last :" << i <<endl; 
 		}
 	}
 	
-	/*for(int i=0; i<n; i++) {
-			cout << arr[i] << " ";
-	}*/
-	cout << counter <<endl;
+	for( int i=0; i<odd; i++) {
+			cout << (char)(car+65);
+	}
+	
+	
+	
+		for(int i=strlen(arr)-1; i>=0; i--) {
+			cout << (arr[i]);
+		}
+	
 	
 	return 0;
 	
